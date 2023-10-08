a4d8455a93b66462c5838cf9ad47351e787a587c
Jerry-Banga <102557931+Jerry-Banga@users.noreply.github.com>
2023-10-08 23:42:21 +0300

    Push to server directory

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+ 
 mongoose.connect("mongodb://127.0.0.1:27017/mails-db", {
     useNewUrlParser: true,
     useUnifiedTopology: true
